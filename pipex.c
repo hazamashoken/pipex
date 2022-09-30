4ccd2014c5efdac48503299ad881fa2286b9f89a
tliangso <earth78203@gmail.com>
2022-10-01 01:31:32 +0800

    add free_child after execve

--- pipex.c
+++ pipex.c
@@ -6,11 +6,13 @@
 /*   By: tliangso <earth78203@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 03:11:10 by tliangso          #+#    #+#             */
-/*   Updated: 2022/10/01 01:04:30 by tliangso         ###   ########.fr       */
+/*   Updated: 2022/10/01 01:31:04 by tliangso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"pipex.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int	err_msg(char *str)
 {
@@ -98,18 +100,19 @@ void	first_child(t_pipex pipex, char **argv, char **envp)
 		pipex.infile = open(argv[1], O_RDONLY);
 		if (pipex.infile < 0)
 			perr_msg(argv[1]);
-		dup2(pipex.tube[1], 1);
 		close(pipex.tube[0]);
-		dup2(pipex.infile, 0);
+		dup2(pipex.tube[1], STDOUT_FILENO);
+		dup2(pipex.infile, STDIN_FILENO);
 		pipex.cmd_args = ft_split(argv[2], ' ');
 		pipex.cmd = get_cmd(pipex.cmd_paths, pipex.cmd_args[0]);
 		if (!pipex.cmd)
 		{
 			free_child(&pipex);
 			err_msg("Command not found\n");
-			exit(1);
+			exit(EXIT_FAILURE);
 		}
 		execve(pipex.cmd, pipex.cmd_args, envp);
+		free_child(&pipex);
 	}
 }
 
@@ -125,18 +128,19 @@ void	second_child(t_pipex pipex, char **argv, char **envp)
 		pipex.outfile = open(argv[4], O_TRUNC | O_CREAT | O_RDWR, 0000644);
 		if (pipex.outfile < 0)
 			perr_msg(argv[4]);
-		dup2(pipex.tube[0], 0);
 		close(pipex.tube[1]);
-		dup2(pipex.outfile, 1);
+		dup2(pipex.tube[0], STDIN_FILENO);
+		dup2(pipex.outfile, STDOUT_FILENO);
 		pipex.cmd_args = ft_split(argv[3], ' ');
 		pipex.cmd = get_cmd(pipex.cmd_paths, pipex.cmd_args[0]);
 		if (!pipex.cmd)
 		{
 			free_child(&pipex);
-			err_msg("Command not found!\n");
-			exit(1);
+			err_msg("Command not found\n");
+			exit(EXIT_FAILURE);
 		}
 		execve(pipex.cmd, pipex.cmd_args, envp);
+		free_child(&pipex);
 	}
 }
 
